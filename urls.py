bd62ee11603c28f502a21af0e3cfdb936691db63
Aaron Pearce <me@aaronpearce.com>
2018-01-12 21:26:23 +1300

    Ordering

--- urls.py
+++ urls.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 from app import views
 
 urlpatterns = [
-    url(r'^verify', views.verify_receipt),
     url(r'^verify_sandbox', views.verify_sandbox_receipt),
+    url(r'^verify', views.verify_receipt),
+    
 ]
 
